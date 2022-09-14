d0c6e5d7e710bbc71945d3d7afbb65fe7ba0c661
imSAJJAKALI <sajjak2506@gmail.com>
2022-09-14 07:55:03 +0530

    not complete code

---
+++ problem.js
@@ -0,0 +1,15 @@
+// Problem 4: Given stored username and password and input username and password, Print if the user can login or not.
+
+let user_name = "sjk@gmail.com";
+let password = 123456;
+
+let correctU = "sjk@gmail.com";
+let correctP = 123456;
+
+if(user_name == correctU){
+  if(password == correctP){
+    console.log("login successful");
+  }else("password is wrong")
+}else{
+console.log("email is credintle");
+}
